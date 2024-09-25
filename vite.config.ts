734ba40dda839fdf1e51243c39ec24b58d3031b7
Sergei Borja <serge.borja0107@gmail.com>
2024-09-25 12:10:38 -0600

    Finished Project.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler'
-      }
+    plugins: [react()],
+    base: '/interactive-pricing-component-main/',
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler'
+            }
+        }
     }
-  }
 })
